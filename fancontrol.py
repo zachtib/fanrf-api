2c4d10d8c92e61715d21bad5756d33156473a4a6
Zach Tibbitts <zachtib@gmail.com>
2017-01-25 09:38:26 -0500

    Comments and type hints

--- fancontrol.py
+++ fancontrol.py
@@ -14,24 +14,25 @@ class FanController:
         self._fan_speed = 0
         self._brightness = 0
 
-    def get_status(self):
+    def get_status(self) -> dict:
         return dict(fan_speed=self._fan_speed, brightness=self._brightness)
 
-    def set_fan_speed(self, fan_speed):
+    def set_fan_speed(self, fan_speed) -> dict:
         if not (0 <= fan_speed <= 3):
             raise FanControlError("Fan speed out of range")
         self._fan_speed = fan_speed
         return self._send()
 
-    def set_brightness(self, brightness):
+    def set_brightness(self, brightness) -> dict:
         if not (0 <= brightness <= 100):
             raise FanControlError("Brightness out of range")
         self._brightness = brightness
         return self._send()
 
-    def _send(self):
+    def _send(self) -> dict:
         print(self._encode())  # TODO: send the state to the RF module here
         return dict(fan_speed=self._fan_speed, brightness=self._brightness, success=True)
 
     def _encode(self) -> str:
+        # TODO: Make this return the correct format
         return '{0:04b}'.format(self._fan_speed) + '{0:08b}'.format(self._brightness)
